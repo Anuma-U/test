a38bc6b6480881d754cf7daa2b15810a97ec38de
Anuma-U <2005.katy.volovlikova@gmail.com>
2024-07-27 11:36:07 +0300

    Add files via upload

--- 7.py
+++ 7.py
@@ -1,13 +1,6 @@
-#1st program
-print((9 ** 0.5) * 5)
-
-#2nd program
-print(9.99 > 9.98 and 1000 != 1000.1)
-
-#3rd program
-print(((1234 % 1000 - 4) + (5678 % 1000 - 8)) // 10)
-
-#4th program
-a = 13.42
-b = 42.13
-print((int((a * 100) // 100) == (b * 100) % 100) or (int((b * 100) // 100) == (a * 100) % 100))
+example = "Notice me sempai, (o)_(o)"
+print(example[0])
+print(example[-1])
+print(example[len(example)//2:])
+print(example[::-1])
+print(example[1::2])
