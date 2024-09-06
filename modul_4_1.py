669abaad6b6cea02ec8d257535ff138db7566ef6
Anuma-U <2005.katy.volovlikova@gmail.com>
2024-09-06 07:45:29 +0300

    Add files via upload

---
+++ modul_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import devide as dev
+from true_math import devide as devi
+
+result1 = dev(69, 3)
+result2 = dev(3, 0)
+result3 = devi(49, 7)
+result4 = devi(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
