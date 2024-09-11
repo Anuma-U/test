b66d017fd129c756f05ea217d3559788ef9388d5
Anuma-U <2005.katy.volovlikova@gmail.com>
2024-09-11 19:33:20 +0300

    Add files via upload

---
+++ class2.1.py
@@ -0,0 +1,67 @@
+class house():
+    def __init__(self, nam, num_floor):
+        self.name = nam
+        self.number_of_floors = num_floor
+
+    def go_to(self, new_flor):
+        self.new_floor = new_flor
+        if 1 < self.new_floor < self.number_of_floors:
+            for i in range(1, self.new_floor + 1):
+                print(i)
+        else:
+            print("Такого этажа не существует")
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+    def __eq__(self, other):
+        return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, other):
+        if isinstance(other, house):
+            self.number_of_floors += other.number_of_floors
+        elif isinstance(other, int):
+            self.number_of_floors += other
+        return self
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __iadd__(self, other):
+        return self.__add__(other)
+
+h1 = house('ЖК Горский', 18)
+h2 = house('Домик в деревне', 2)
+print(h1)
+print(h2)
+print(h1 == h2)
+h1 = h1 + 10
+print(h1)
+print(h1 == h2)
+h1 += 10
+print(h1)
+h2 = 10 + h2
+print(h2)
+print(h1 > h2)
+print(h1 >= h2)
+print(h1 < h2)
+print(h1 <= h2)
+print(h1 != h2)
