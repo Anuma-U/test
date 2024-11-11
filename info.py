cfe4a8137c72ca192fcb77fef2099cb1896b32d1
Anuma-U <2005.katy.volovlikova@gmail.com>
2024-11-11 22:30:19 +0300

    info.py

--- info.py
+++ info.py
@@ -14,21 +14,7 @@ def introspection_info(obj):
     info = {"type": str(type_obj), "attributes":atribut, "methods": methods, "module": modul}
     return info
 
-# здесь класс для того чтобы проверить другую часть функции(else)
-# class Cucumber:
-#     def __init__(self):
-#         self.aaa = 12
-#         self.a = 56
-#
-#     def plus(self):
-#         return self.aaa + self.a
-#
-#     def minus(self):
-#         return self.a - self.aaa
-#
-#
-# fakt = Cucumber()
-# print(introspection_info(fakt))
+
 number_info = introspection_info(42)
 print(number_info)
 
